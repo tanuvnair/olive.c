b894eaee39bbade4f80b8def559030bbabf6fba7
Tanuv Nair <tanuvnair@gmail.com>
2023-12-01 18:00:54 +0530

    Added checker example

--- example.c
+++ example.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <string.h>
 #include "olive.c"
@@ -8,24 +9,41 @@
 #define WIDTH 800
 #define HEIGHT 600
 
+#define COLS 8
+#define ROWS 6
+#define CELL_WIDTH (WIDTH/COLS)
+#define CELL_HEIGHT (HEIGHT/ROWS)
+
+#define BACKGROUND_COLOR 0xFF202020
+
 // Unsigned 32-bit integer which stores the pixels
 static uint32_t pixels[HEIGHT * WIDTH];
 
-int main(void) {
-    olivec_fill(pixels, WIDTH, HEIGHT, 0xFF202020);
+bool checker_example(void) {
+    olivec_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
 
-    size_t rw = 50*4;
-    size_t rh = 30*4;
+    for (int y = 0; y < ROWS; ++y) {
+        for (int x = 0; x < COLS; ++x) {
+            uint32_t color = BACKGROUND_COLOR;
+            if ((x + y) % 2 == 0) {
+                color = 0xFF2020FF;
+            }
+            olivec_fill_rect(pixels, WIDTH, HEIGHT, x*CELL_WIDTH, y*CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, color);
+        }
+    }
 
-    olivec_fill_rect(pixels, WIDTH, HEIGHT, WIDTH/2 - rw/2, HEIGHT/2 - rh/2, rw, rh, 0xFF2020FF);
-
-    const char *file_path = "output.ppm";
+    const char *file_path = "checker.ppm";
 
     Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
     if (err) {
         fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(errno));
-        return 1;
+        return false;
     }
- 
+
+    return true;
+}
+
+int main(void) {
+    checker_example();
     return 0;
 }
