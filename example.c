a120c1051ad30e3f25da3d1aa3aa0260d554fb88
Tanuv Nair <tanuvnair@gmail.com>
2023-12-11 06:46:30 +0530

    Added function for creating circle and a circle example.

--- example.c
+++ example.c
@@ -15,6 +15,7 @@
 #define CELL_HEIGHT (HEIGHT/ROWS)
 
 #define BACKGROUND_COLOR 0xFF202020
+#define FOREGROUND_COLOR 0xFF2020FF
 
 // Unsigned 32-bit integer which stores the pixels
 static uint32_t pixels[HEIGHT * WIDTH];
@@ -43,7 +44,31 @@ bool checker_example(void) {
     return true;
 }
 
+bool circle_example(void) {
+    olivec_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
+
+    for (int y = 0; y < ROWS; y++) {
+        for (int x = 0; x < COLS; x++) {
+            size_t radius = CELL_WIDTH;
+
+            if (CELL_HEIGHT < radius) radius = CELL_HEIGHT;
+            olivec_fill_circle(pixels, WIDTH, HEIGHT, x*CELL_WIDTH + CELL_WIDTH/2, y*CELL_HEIGHT + CELL_HEIGHT/2, radius/2, FOREGROUND_COLOR);
+        }
+    }
+
+    const char *file_path = "circle.ppm";
+    Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
+    if (err) {
+        fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(errno));
+        return false;
+    }
+
+    return true;
+}
+
 int main(void) {
-    checker_example();
+    if (!checker_example()) return -1;
+    if (!circle_example()) return -1;
+
     return 0;
 }
--- olive.c
+++ olive.c
@@ -56,4 +56,27 @@ void olivec_fill_rect(uint32_t *pixels, size_t pixels_width, size_t pixels_heigh
     }
 }
 
+void olivec_fill_circle(uint32_t *pixels, size_t pixels_width, size_t pixels_height, int cx, int cy, size_t r, uint32_t color) {
+    int x1 = cx - (int) r;
+    int y1 = cy - (int) r;
+    int x2 = cx + (int) r;
+    int y2 = cy + (int) r;
+
+    for (int y = y1; y <= y2; y++) {
+        if (y >= 0 && y < (int) pixels_height) {
+            for (int x = x1; x <= x2; x++) {
+                if (x >= 0 && x < (int) pixels_width) {
+                    int dx = x - cx;
+                    int dy = y - cy;
+                    
+                    // sqrt(dx*dx + dy*dy) <= r*r
+                    if (dx*dx + dy*dy <= r*r) {
+                        pixels[y*pixels_width + x] = color;
+                    }
+                }
+            }
+        }
+    }
+}
+
 #endif // OLIVE_C_
