d6ad1934124f528b9cd01726b14fe8ca3fde82ce
Tanuv Nair <tanuvnair@gmail.com>
2024-05-07 02:12:56 +0530

    Added olivec_draw_line() function and lines_examples()

--- example.c
+++ example.c
@@ -11,9 +11,8 @@
 
 #define COLS 8
 #define ROWS 6
-#define CELL_WIDTH (WIDTH/COLS)
-#define CELL_HEIGHT (HEIGHT/ROWS)
-
+#define CELL_WIDTH (WIDTH / COLS)
+#define CELL_HEIGHT (HEIGHT / ROWS)
 
 #define BACKGROUND_COLOR 0xFF202020
 #define FOREGROUND_COLOR 0xFF2020FF
@@ -21,27 +20,97 @@
 // Unsigned 32-bit integer which stores the pixels
 static uint32_t pixels[HEIGHT * WIDTH];
 
-void olivec_draw_line(uint32_t *pixels, size_t pixels_width, size_t pixels_height, int x1, int y1, int x2, int y2, uint32_t color) {
-    
+void swap_int(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
 
-bool checker_example(void) {
+void olivec_draw_line(uint32_t *pixels, size_t pixels_width,
+                      size_t pixels_height, int x1, int y1, int x2, int y2,
+                      uint32_t color)
+{
+    // Objective is to get the values of k and c, where k represents the slope and c is the y-intercept.
+    // y = k*x + c
+    // y1 = k*x1 + c -(i)
+    // y2 = k*x2 + c -(ii)
+    // c = y1 - k*x1 -(iii)
+
+    // After Substituting (iii) in (ii)
+    // y2 = k*x2 + y1 - k*x1
+    // y2 = k*(x2 - x1) + y1
+    // y2 - y1 = k*(x2 - x1)
+    /// k = (y2 - y1)/(x2 - x1)
+
+    // c = y1 - k*x1
+    // k = (y2 - y1)/(x2 - x1)
+
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    if (dx != 0)
+    {
+        int c = y1 - dy * x1 / dx;
+
+        if (x1 > x2)
+            swap_int(&x1, &x2);
+        for (int x = x1; x <= x2; ++x)
+        {
+            if (0 <= x && x < (int) pixels_width)
+            {
+                int sy1 = dy * x / dx + c;
+                int sy2 = dy * (x + 1) / dx + c;
+                if (sy1 > sy2) swap_int(&sy1, &sy2);
+                for (int y = sy1; y <= sy2; y++) {
+                    if (0 <= y && y < (int) pixels_height)
+                    {
+                        pixels[y * pixels_width + x] = color;
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        int x = x1;
+        if (0 <= x && x < (int) pixels_width)
+        {
+            if (y1 > y2)
+                swap_int(&y1, &y2);
+            for (int y = y1; y <= y2; ++y)
+            {
+                if (0 <= y && y < (int) pixels_height)
+                {
+                    pixels[y * pixels_width + x] = color;
+                }
+            }
+        }
+    }
+}
+
+bool checker_example(void)
+{
     olivec_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
 
-    for (int y = 0; y < ROWS; ++y) {
-        for (int x = 0; x < COLS; ++x) {
+    for (int y = 0; y < ROWS; ++y)
+    {
+        for (int x = 0; x < COLS; ++x)
+        {
             uint32_t color = BACKGROUND_COLOR;
-            if ((x + y) % 2 == 0) {
+            if ((x + y) % 2 == 0)
+            {
                 color = 0xFF2020FF;
             }
-            olivec_fill_rect(pixels, WIDTH, HEIGHT, x*CELL_WIDTH, y*CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, color);
+            olivec_fill_rect(pixels, WIDTH, HEIGHT, x * CELL_WIDTH, y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, color);
         }
     }
 
     const char *file_path = "checker.ppm";
 
     Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
-    if (err) {
+    if (err)
+    {
         fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(errno));
         return false;
     }
@@ -49,28 +118,34 @@ bool checker_example(void) {
     return true;
 }
 
-float lerpf(float a, float b, float t) {
-    return a + (b - a)*t;
+float lerpf(float a, float b, float t)
+{
+    return a + (b - a) * t;
 }
 
-bool circle_example(void) {
+bool circle_example(void)
+{
     olivec_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
 
-    for (int y = 0; y < ROWS; y++) {
-        for (int x = 0; x < COLS; x++) {
-            float u = (float)x/COLS;
-            float v = (float)y/ROWS;
-            float t = (u + v)/2;
-            
+    for (int y = 0; y < ROWS; y++)
+    {
+        for (int x = 0; x < COLS; x++)
+        {
+            float u = (float)x / COLS;
+            float v = (float)y / ROWS;
+            float t = (u + v) / 2;
+
             size_t radius = CELL_WIDTH;
-            if (CELL_HEIGHT < radius) radius = CELL_HEIGHT;
-            olivec_fill_circle(pixels, WIDTH, HEIGHT, x*CELL_WIDTH + CELL_WIDTH/2, y*CELL_HEIGHT + CELL_HEIGHT/2, (size_t) lerpf(radius/8, radius/2, t),FOREGROUND_COLOR);
+            if (CELL_HEIGHT < radius)
+                radius = CELL_HEIGHT;
+            olivec_fill_circle(pixels, WIDTH, HEIGHT, x * CELL_WIDTH + CELL_WIDTH / 2, y * CELL_HEIGHT + CELL_HEIGHT / 2, (size_t)lerpf(radius / 8, radius / 2, t), FOREGROUND_COLOR);
         }
     }
 
     const char *file_path = "circle.ppm";
     Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
-    if (err) {
+    if (err)
+    {
         fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(errno));
         return false;
     }
@@ -78,9 +153,43 @@ bool circle_example(void) {
     return true;
 }
 
-int main(void) {
-    if (!checker_example()) return -1;
-    if (!circle_example()) return -1;
+bool lines_example(void)
+{
+    olivec_fill(pixels, WIDTH, HEIGHT, BACKGROUND_COLOR);
 
+    olivec_draw_line(pixels, WIDTH, HEIGHT, 0, 0, WIDTH, HEIGHT, FOREGROUND_COLOR);
+
+    olivec_draw_line(pixels, WIDTH, HEIGHT, WIDTH, 0, 0, HEIGHT, FOREGROUND_COLOR);
+
+    olivec_draw_line(pixels, WIDTH, HEIGHT, 0, 0, WIDTH/4, HEIGHT, 0xFF20FF20);
+
+    olivec_draw_line(pixels, WIDTH, HEIGHT, WIDTH/4, 0, 0, HEIGHT, 0xFF20FF20);
+
+    olivec_draw_line(pixels, WIDTH, HEIGHT, WIDTH, 0, WIDTH/4*3, HEIGHT, 0xFF20FF20);
+
+    olivec_draw_line(pixels, WIDTH, HEIGHT, WIDTH/4*3, 0, WIDTH, HEIGHT, 0xFF20FF20);
+    
+    olivec_draw_line(pixels, WIDTH, HEIGHT, 0, HEIGHT/2, WIDTH, HEIGHT/2, 0xFFFF2020);
+
+    olivec_draw_line(pixels, WIDTH, HEIGHT, WIDTH/2, 0, WIDTH/2, HEIGHT, 0xFFFF2020);
+
+    const char *file_path = "lines.ppm";
+    Errno err = olivec_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
+    if (err)
+    {
+        fprintf(stderr, "ERROR: could not save file %s: %s\n", file_path, strerror(errno));
+        return false;
+    }
+    return true;
+}
+
+int main(void)
+{
+    if (!checker_example())
+        return -1;
+    if (!circle_example())
+        return -1;
+    if (!lines_example())
+        return -1;
     return 0;
 }
--- olive.c
+++ olive.c
@@ -1,38 +1,50 @@
 #ifndef OLIVE_C_
 #define OLIVE_C_
 
-void olivec_fill(uint32_t *pixels, size_t width, size_t height, uint32_t color) {
-    for (size_t i = 0; i < width * height; ++i) {
+void olivec_fill(uint32_t *pixels, size_t width, size_t height, uint32_t color)
+{
+    for (size_t i = 0; i < width * height; ++i)
+    {
         pixels[i] = color;
     }
 }
 
 typedef int Errno;
 
-#define return_defer(value) do {result = (value); goto defer; } while (0)
+#define return_defer(value) \
+    do                      \
+    {                       \
+        result = (value);   \
+        goto defer;         \
+    } while (0)
 
-Errno olivec_save_to_ppm_file(uint32_t *pixels, size_t width, size_t height, const char *file_path) {
+Errno olivec_save_to_ppm_file(uint32_t *pixels, size_t width, size_t height, const char *file_path)
+{
     int result = 0;
     FILE *f = NULL;
 
     {
         f = fopen(file_path, "wb");
-        if (f == NULL) return_defer(errno);
+        if (f == NULL)
+            return_defer(errno);
 
         fprintf(f, "P6\n%zu %zu 255\n", width, height);
-        if (ferror(f)) return_defer(errno);
+        if (ferror(f))
+            return_defer(errno);
 
-        for (size_t i = 0; i < width*height; ++i) {
+        for (size_t i = 0; i < width * height; ++i)
+        {
             // 0xAABBGGRR
             uint32_t pixel = pixels[i];
             uint8_t bytes[3] = {
-                (pixel>>(8*0))&0xFF,
-                (pixel>>(8*1))&0xFF,
-                (pixel>>(8*2))&0xFF,
+                (pixel >> (8 * 0)) & 0xFF,
+                (pixel >> (8 * 1)) & 0xFF,
+                (pixel >> (8 * 2)) & 0xFF,
             };
 
             fwrite(bytes, sizeof(bytes), 1, f);
-            if (ferror(f)) return_defer(errno);
+            if (ferror(f))
+                return_defer(errno);
         }
     }
 
@@ -42,36 +54,47 @@ defer:
     return 0;
 }
 
-void olivec_fill_rect(uint32_t *pixels, size_t pixels_width, size_t pixels_height, int x0, int y0, size_t w, size_t h, uint32_t color) {
-    for (int dy = 0; dy < (int) h; ++dy) {
+void olivec_fill_rect(uint32_t *pixels, size_t pixels_width, size_t pixels_height, int x0, int y0, size_t w, size_t h, uint32_t color)
+{
+    for (int dy = 0; dy < (int)h; ++dy)
+    {
         int y = y0 + dy;
-        if (y >= 0 && y < (int) pixels_height) {
-            for (int dx = 0; dx < (int) w; ++dx) {
+        if (y >= 0 && y < (int)pixels_height)
+        {
+            for (int dx = 0; dx < (int)w; ++dx)
+            {
                 int x = x0 + dx;
-                if (x >= 0 && x < (int) pixels_width) {
-                    pixels[y*pixels_width + x] = color;
+                if (x >= 0 && x < (int)pixels_width)
+                {
+                    pixels[y * pixels_width + x] = color;
                 }
             }
         }
     }
 }
 
-void olivec_fill_circle(uint32_t *pixels, size_t pixels_width, size_t pixels_height, int cx, int cy, size_t r, uint32_t color) {
-    int x1 = cx - (int) r;
-    int y1 = cy - (int) r;
-    int x2 = cx + (int) r;
-    int y2 = cy + (int) r;
+void olivec_fill_circle(uint32_t *pixels, size_t pixels_width, size_t pixels_height, int cx, int cy, size_t r, uint32_t color)
+{
+    int x1 = cx - (int)r;
+    int y1 = cy - (int)r;
+    int x2 = cx + (int)r;
+    int y2 = cy + (int)r;
 
-    for (int y = y1; y <= y2; y++) {
-        if (y >= 0 && y < (int) pixels_height) {
-            for (int x = x1; x <= x2; x++) {
-                if (x >= 0 && x < (int) pixels_width) {
+    for (int y = y1; y <= y2; y++)
+    {
+        if (y >= 0 && y < (int)pixels_height)
+        {
+            for (int x = x1; x <= x2; x++)
+            {
+                if (x >= 0 && x < (int)pixels_width)
+                {
                     int dx = x - cx;
                     int dy = y - cy;
-                    
+
                     // sqrt(dx*dx + dy*dy) <= r*r
-                    if (dx*dx + dy*dy <= r*r) {
-                        pixels[y*pixels_width + x] = color;
+                    if (dx * dx + dy * dy <= r * r)
+                    {
+                        pixels[y * pixels_width + x] = color;
                     }
                 }
             }
